9f2e15f5b0db4bf8fda669c872e7ce31838175d1
Niranjan Mishra <nm4883143@gmail.com>
2025-09-28 20:53:36 +0530

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,15 @@
+import yt_dlp
+
+def download_video(url):
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'merge_output_format': 'mp4',
+        'outtmpl': '%(title)s.%(ext)s'
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    link = input("Enter video URL (YouTube/Facebook/Instagram): ")
+    download_video(link)
+    print("✅ Download complete!")
